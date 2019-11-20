53fe82f23be7afcc6bb5607d38dbd16ac3e7592c
wouyang628 <36004307+wouyang628@users.noreply.github.com>
2019-11-20 15:45:14 -0500

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,4 +1,4 @@
-#sample sample sample
+#EXAMPLE ONLY!
 
 from kafka import KafkaConsumer
 from json import loads
@@ -9,16 +9,21 @@ consumer = KafkaConsumer(
     'eventService',
      bootstrap_servers=['localhost:9092'],
      auto_offset_reset='earliest',
-     value_deserializer=lambda x: loads(x.decode('utf-8')),
+     value_deserializer=lambda m: m.decode('utf-8'),
      enable_auto_commit=True,
      group_id='my-group')
 
-es = Elasticsearch([{"host": "localhost", "port": 9200}])
+#es = Elasticsearch([{"host": "localhost", "port": 9200}])
+
 for message in consumer:
     message = message.value
     print(message)
-    print(message["error_message"])
-    es.index(index='events', body = event1)
+    try:
+        message = json.loads(message)
+        print(message["error_message"])
+        #es.index(index='events', body = event1)
+    except:
+        print("not valid")
     
 """
 e.g
